0e61ba3dbaab3ba3f11baea7171ea73107934c5e
Charles Dion-Buteau <charles.dion-buteau.1@ulaval.ca>
2024-07-27 10:18:18 -0400

    modifed sample and iteration functions

--- pagerank.py
+++ pagerank.py
@@ -2,7 +2,6 @@ import os
 import random
 import re
 import sys
-import time
 
 DAMPING = 0.85
 SAMPLES = 10000
@@ -68,22 +67,22 @@ def transition_model(corpus, page, damping_factor):
     # Number of sites in corpus
     num_elem = len(corpus)
 
-    probDist = {}
+    probDistr = {}
 
     if linkNumber == 0:
         #If the page has no link, we return a dict of site with all the same probability
         prob = 1/num_elem
-        for site in page:
-            probDist[site] = prob
+        for site in corpus:
+            probDistr[site] = prob
         print('Should not be there')
     else:
         for site in corpus:
             if site in corpus[page]:
-                probDist[site] = damping_factor*(1/linkNumber) + ((1-damping_factor)/num_elem)
+                probDistr[site] = damping_factor*(1/linkNumber) + ((1-damping_factor)/num_elem)
             else:
-                probDist[site] = (1-damping_factor)/num_elem
+                probDistr[site] = (1-damping_factor)/num_elem
 
-    return probDist
+    return probDistr
 
 def sample_pagerank(corpus, damping_factor, n):
     """
@@ -150,7 +149,98 @@ def iterate_pagerank(corpus, damping_factor):
     their estimated PageRank value (a value between 0 and 1). All
     PageRank values should sum to 1.
     """
-    raise NotImplementedError
+    # Creation of the initial page rank dictionary
+    PageRankDict = {}
+    # Creation of the dictionary to calculate the max delta values during the iteration
+    PreviousPageRankDict = {}
+    for site in corpus.keys():
+        PageRankDict[site] = 1/len(corpus)
+        PreviousPageRankDict[site] = 1/len(corpus)
+
+    # Find the number of links pointing to each page
+
+    reverseCorpus = {}
+
+    numLinks = {}
+
+    for page in corpus:
+        reverseCorpus[page] = set()
+        numLinks[page] = 0
+
+    for page in corpus:
+        for link in corpus:
+            if page in corpus[link]:
+                reverseCorpus[page].add(link)
+                numLinks[page] += 1
+    print(corpus)
+    print(reverseCorpus)
+
+    #PreviousPageRankDict = PageRankDict
+    #print(PreviousPageRankDict)
+    maxDelta = float('inf')
+    # Each iteration of the while loop, we will check for the max delta value between the last results and
+    # the new. When this max value will be < 0.001, we converge
+    #print(corpus)
+    iteration = 0
+    
+    while maxDelta > 0.001:
+        iteration += 1
+    # Iterage on each page and calculae the new pageRange
+        
+        for page in corpus:
+            #print(page)
+            # lets find the number of link that links to p
+            #numLinks = 0
+            #for site in corpus:
+                #if page in corpus[site]:
+                    #numLinks += 1
+
+           #numLinks = len(reverseCorpus[page])
+            
+
+            #numLinks = len(corpus[page])
+            # If number of links is zero, numlink = nb pages in coprpus
+            if numLinks == 0:
+                numLinks = len(corpus)
+                print("Should not appear")
+                # Lets first calcullate the sumation part
+                sumPart = 0
+                for i in corpus:
+                    sumPart += PageRankDict[i]/numLinks
+                
+                PageRankDict[page] = (1-damping_factor)/len(corpus) + (damping_factor*sumPart)
+            else:
+                #print(f"Previous1 : {PreviousPageRankDict}")
+                
+                #print(numLinks)
+                #print(len(corpus))
+                sumPart = 0
+                for i in corpus[page]:
+                    sumPart += PreviousPageRankDict[i]/numLinks[i]
+                #print((1-damping_factor)/len(corpus) + (damping_factor*sumPart))
+                #print(f'res = {damping_factor*sumPart} + {(1-damping_factor)/len(corpus)}')
+                PageRankDict[page] = (((1-damping_factor)/len(corpus)) + (damping_factor*sumPart))
+                #print(PageRankDict[page])
+        # Find the maximum deltaValue
+        #print(f"Previous2 : {PreviousPageRankDict}")
+        #print(PageRankDict)
+        #print(f"iteration #{iteration}")
+        maxDelta = 0
+        
+        for j in PageRankDict:
+            currentDelta = PageRankDict[j] - PreviousPageRankDict[j]
+            print(f'{currentDelta} = {PageRankDict[j]} - {PreviousPageRankDict[j]}')
+            if abs(currentDelta) > abs(maxDelta):
+                maxDelta = abs(currentDelta)
+        print(maxDelta)
+        #PreviousPageRankDict = PageRankDict
+        for site in corpus.keys():
+            PreviousPageRankDict[site] = PageRankDict[site]
+
+            
+                    
+    return PageRankDict
+
 
 
 if __name__ == "__main__":
